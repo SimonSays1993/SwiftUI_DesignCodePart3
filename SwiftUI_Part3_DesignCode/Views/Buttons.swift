d60cf06bd7f664ffb43ea47badbcf170af6c0cec
Simon McNeil <simonmcneil062@gmail.com>
2022-02-01 00:03:46 -0500

    Added a tap gesture to button and animation interactions

--- SwiftUI_Part3_DesignCode/Views/Buttons.swift
+++ SwiftUI_Part3_DesignCode/Views/Buttons.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct Buttons: View {
+    @State var tap = false
+    @State var press = false
+    
     var body: some View {
         VStack {
             Text("Button")
@@ -15,9 +18,15 @@ struct Buttons: View {
                 .frame(width: 200, height: 60)
                 .background(
                     ZStack {
-                        Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
+                        Color(press ? .white : #colorLiteral(red: 0.7608050108,
+                                                             green: 0.8164883852,
+                                                             blue: 0.9259157777,
+                                                             alpha: 1))
                         RoundedRectangle(cornerRadius: 16, style: .continuous)
-                            .foregroundColor(Color.white)
+                            .foregroundColor(Color(press ? #colorLiteral(red: 0.7608050108,
+                                                                         green: 0.8164883852,
+                                                                         blue: 0.9259157777,
+                                                                         alpha: 1) : .white))
                             .blur(radius: 4)
                             .offset(x: -8, y: -8)
                         
@@ -31,12 +40,44 @@ struct Buttons: View {
                     }
                 )
                 .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .shadow(color: Color(#colorLiteral(red: 0.7608050108,
-                                                   green: 0.8164883852,
-                                                   blue: 0.9259157777,
-                                                   alpha: 1)), radius: 20, x: 20, y: 20)
-                .shadow(color: Color(.white), radius: 20, x: -20, y: 20)
+                .overlay(
+                    HStack {
+                        Image(systemName: "person.crop.circle")
+                            .font(.system(size: 24, weight: .light))
+                            .foregroundColor(
+                                Color.white.opacity(press ? 0 : 1)
+                            )
+                            .frame(width: press ? 64 : 54, height: press ? 4: 50)
+                            .background(Color(hue: 0.692, saturation: 0.985, brightness: 1.0))
+                            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                            .shadow(color: Color(hue: 0.692, saturation: 0.985, brightness: 1.0).opacity(0.3),
+                                radius: 10, x: 10, y: 10)
+                            .offset(x: press ? 70 : -10, y: press ? 16 : 0)
+                            .animation(.linear(duration: 0.15))
+                        Spacer()
+                    }
+                )
+                .shadow(color: Color(press ? .white : #colorLiteral(red: 0.7608050108,
+                                                                    green: 0.8164883852,
+                                                                    blue: 0.9259157777,
+                                                                    alpha: 1)), radius: 20, x: 20, y: 20)
+                .shadow(color: Color(press ? #colorLiteral(red: 0.7608050108,
+                                                           green: 0.8164883852,
+                                                           blue: 0.9259157777,
+                                                           alpha: 1) : .white ), radius: 20, x: -20, y: 20)
+                .scaleEffect(tap ? 1.2 : 1)
+                .gesture(
+                    LongPressGesture(minimumDuration: 0.5).onChanged { value in
+                        self.tap = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            self.tap = false
+                        }
+                    }.onEnded{ value in
+                        self.press.toggle()
+                    }
+                )
         }
+        .animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
         .edgesIgnoringSafeArea(.all)
